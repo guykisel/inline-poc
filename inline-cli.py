3f6d0b8786b7e8dbea26e5e249b292c7c631a71b
Guy Kisel <guy.kisel@gmail.com>
2015-12-31 15:26:28 -0800

    initial skeleton

--- inline-cli.py
+++ inline-cli.py
@@ -4,8 +4,24 @@ import argparse
 
 import requests
 
-def main():
+def inline(filename, owner, repo, pr, auth):
+    messages = parse(filename)
+    post_comments(messages, owner, repo, pr, auth)
+
+def parse(filename):
+    messages = []
+    return messages
+
+def post_comments(messages, owner, repo, pr, auth):
     pass
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pr', type=int, required=True)
+    parser.add_argument('--owner', type=str, required=True)
+    parser.add_argument('--repo', type=str, required=True)
+    parser.add_argument('--auth', type=str, required=True)
+    parser.add_argument('--filename', type=str, required=True)
+
+    args = parser.parse_args()
+    inline(args.filename, args.owner, args.repo, args.pr, args.auth)
