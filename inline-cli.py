6d260e59290b688e5f5a73dce624842da39f1deb
Guy Kisel <guy.kisel@gmail.com>
2016-01-03 14:31:44 -0800

    use unidiff to determine positions

--- inline-cli.py
+++ inline-cli.py
@@ -5,27 +5,38 @@ import pprint
 import subprocess
 
 import github3
+import unidiff
 
 
 def inline(filename, owner, repo, pr, user, token):
-    print(token)
-    #token = token.encode('utf-8')
-    print(token)
     gh = github3.GitHub(user, token=token)
-    messages = parse(filename)
-    post_comments(messages, owner, repo, pr, gh)
+
+    pull_request = gh.pull_request(owner, repo, pr)
+    sha = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode('utf-8')
+    diff = pull_request.diff()
+
+    messages = parse(filename, diff)
+    post_comments(messages, pull_request, sha)
 
 
-def message(filename, line, content):
+def message(filename, line, content, diff):
     if content:
-        return {
-            'filename': filename,
-            'line': line,
-            'content': 'Line: ' + str(line) + ' \n```\n' + content.strip() + '\n```'
-        }
+        patch = unidiff.PatchSet(diff.decode('utf-8').split('\n'))
+        for patched_file in patch:
+            if patched_file.target_file == 'b/' + filename:
+                for hunk in patched_file:
+                    for position, hunk_line in enumerate(hunk):
+                        if '+' not in hunk_line.line_type:
+                            continue
+                        if hunk_line.target_line_no == line:
+                            return {
+                                'filename': filename,
+                                'line': position,
+                                'content': 'Line: ' + str(line) + ' \n```\n' + content.strip() + '\n```'
+                            }
 
 
-def parse(filename):
+def parse(filename, diff):
     messages = []
     current_filename = ''
     current_line = ''
@@ -34,14 +45,14 @@ def parse(filename):
         for line in infile:
             # new filename
             if not line.startswith(' '):
-                messages.append(message(current_filename, current_line, current_message_content))
+                messages.append(message(current_filename, current_line, current_message_content, diff))
                 current_filename = line.strip()
                 current_line = ''
                 current_message_content = ''
                 continue
             # new line number
             elif not line.startswith('    '):
-                messages.append(message(current_filename, current_line, current_message_content))
+                messages.append(message(current_filename, current_line, current_message_content, diff))
                 current_line = int(line.replace('  Line: ', '').strip())
                 current_message_content = ''
                 continue
@@ -51,14 +62,11 @@ def parse(filename):
     return messages
 
 
-def post_comments(messages, owner, repo, pr, gh):
-    pull_request = gh.pull_request(owner, repo, pr)
-    sha = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode('utf-8')
-    print(sha)
+def post_comments(messages, pull_request, sha):
     for msg in messages:
-        if msg:
-            pprint.pprint(msg)
-            pull_request.create_review_comment(msg['content'], sha, msg['filename'], msg['line'])
+        if not msg:
+            continue
+        pull_request.create_review_comment(msg['content'], sha, msg['filename'], msg['line'])
 
 
 if __name__ == "__main__":
