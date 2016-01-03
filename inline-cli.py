a3d75f6e1624ae5b403ebcec05c9e13127d58c3d
Guy Kisel <guy.kisel@gmail.com>
2016-01-03 13:14:21 -0800

    comment using messages

--- inline-cli.py
+++ inline-cli.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 import argparse
+import subprocess
 
-import requests
+import github3
 
 
-def inline(filename, owner, repo, pr, auth):
+def inline(filename, owner, repo, pr, user, token):
+    gh = github3.login(user, token=token)
     messages = parse(filename)
-    post_comments(messages, owner, repo, pr, auth)
+    post_comments(messages, owner, repo, pr, gh)
 
 
 def message(filename, line, content):
@@ -44,8 +46,12 @@ def parse(filename):
     return messages
 
 
-def post_comments(messages, owner, repo, pr, auth):
-    pass
+def post_comments(messages, owner, repo, pr, gh):
+    pull_request = gh.pull_request(owner, repo, pr)
+    sha = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+    for msg in messages:
+        if msg:
+            pull_request.create_review_comment(msg['content'], sha, msg['filename'], msg['line'])
 
 
 if __name__ == "__main__":
@@ -53,8 +59,9 @@ if __name__ == "__main__":
     parser.add_argument('--pr', type=int, required=True)
     parser.add_argument('--owner', type=str, required=True)
     parser.add_argument('--repo', type=str, required=True)
-    parser.add_argument('--auth', type=str, required=True)
+    parser.add_argument('--token', type=str)
+    parser.add_argument('--user', type=str)
     parser.add_argument('--filename', type=str, required=True)
 
     args = parser.parse_args()
-    inline(args.filename, args.owner, args.repo, args.pr, args.auth)
+    inline(args.filename, args.owner, args.repo, args.pr, args.user, args.token)
