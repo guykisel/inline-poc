e86897c0427be4fb9ea0f0d5d8384d38c4720dd1
Guy Kisel <guy.kisel@gmail.com>
2016-01-03 13:03:21 -0800

    cleanup

--- inline-cli.py
+++ inline-cli.py
@@ -4,18 +4,19 @@ import argparse
 
 import requests
 
+
 def inline(filename, owner, repo, pr, auth):
     messages = parse(filename)
     post_comments(messages, owner, repo, pr, auth)
 
-def add_message(messages, filename, line, content):
-    if content.strip():
-        message = {
-            'filename': filename,
-            'line': line,
-            'content': content
-        }
-        messages.append(message)
+
+def message(filename, line, content):
+    return {
+        'filename': filename,
+        'line': line,
+        'content': content
+    }
+
 
 def parse(filename):
     messages = []
@@ -26,14 +27,14 @@ def parse(filename):
         for line in infile:
             # new filename
             if not line.startswith(' '):
-                add_message(messages, current_filename, current_line, current_message_content)
+                messages.append(message(current_filename, current_line, current_message_content))
                 current_filename = line.strip()
                 current_line = ''
                 current_message_content = ''
                 continue
             # new line number
             elif not line.startswith('    '):
-                add_message(messages, current_filename, current_line, current_message_content)
+                messages.append(message(current_filename, current_line, current_message_content))
                 current_line = int(line.replace('  Line: ', '').strip())
                 current_message_content = ''
                 continue
@@ -42,8 +43,10 @@ def parse(filename):
 
     return messages
 
+
 def post_comments(messages, owner, repo, pr, auth):
     pass
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
